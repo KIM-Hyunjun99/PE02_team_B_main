8b040c53bc1f4760820e73f059393227ab1a7372
J Seo <seojy06@hanyang.ac.kr>
2023-05-07 17:30:40 +0900

    function change

--- src/functions.py
+++ src/functions.py
@@ -61,6 +61,31 @@ def Ref_fitted_data(X,Y): # 가장 R_square가 클 때의 fitting data를 반환
     max_degree = Rs.index(max(Rs))+1
     return fit_data(X,Y,max_degree)
 
+def shockely_diode_IV_fit(V,I):
+    def shockely_diode(voltage, rev_sat_I, n):
+        k = 1.380649 * 10 ** (-23)
+        q = 1.602 * 10 ** (-19)
+        temp = 300
+        return rev_sat_I * (np.exp(q * voltage / (n * k * temp)) - 1)
+
+    # 모델 인스턴스 생성
+    model = Model(shockely_diode)
+
+    # 초기 매개 변수 설정
+    params = model.make_params(
+        rev_sat_I=1e-7,
+        n=1
+    )
+
+    # 모델 피팅
+    result = model.fit(I[10:], params, voltage=V[10:])
+    # print(result.best_fit,'\n',result.best_values) # parameter 값과 근사된 데이터에 대한 값을 보여주는 코드
+    coef = np.polyfit(V[:10],I[:10],9)
+    func = np.poly1d(coef)
+    fit_data = func(V[:10])
+    fit_data = np.append(fit_data, result.best_fit)
+    return fit_data
+
 def flat_fit_function(X,Y): # R_square가 가장 클 때의 근사 함수를 반환하는 함수
     Rs = []
     for i in range(1, 11):
