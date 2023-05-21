e5c595b5e692bb66e8385ddff255b6c90383b702
J Seo <seojy06@hanyang.ac.kr>
2023-05-21 17:59:38 +0900

    default code

--- src/graph_individual.py
+++ src/graph_individual.py
@@ -75,7 +75,7 @@ def transmission_spectra(A, X, Y, Z):
     line, = plt.plot(v[6][0], v[6][1], color='gray', label="REF")  # REF data plot
 
     plt.gca().add_artist(plt.legend(handles=[line], loc='upper right', fontsize=7))  # REF 레이블을 추가
-    plt.legend(handles=plots, ncol=3, loc="lower center", fontsize=7)  # 나머지 레이블을 추가
+    plt.legend(handles=plots, ncol=3, loc="lower left", fontsize=5)  # 나머지 레이블을 추가
     plt.title("Transmission spectra", fontdict={'weight': 'bold', 'size': 10})  # 그래프 제목을 설정
     plt.xticks(fontsize=6)  # modulate axis label's fontsize
     plt.yticks(fontsize=6)
@@ -106,7 +106,7 @@ def transmission_rsquare(A,X,Y,Z):
     for i in range(1, 11):
         z = np.polyfit(v[6][0], v[6][1], i)
         p = np.poly1d(z)
-        line, = plt.plot(v[6][0], p(v[6][0]), label=str(i) + 'degree')
+        line, = plt.plot(v[6][0], p(v[6][0]), label=str(i) + 'deg')
         plots.append(line)
         a = np.array(v[6][0])
         b = np.array(v[6][1])
@@ -119,20 +119,20 @@ def transmission_rsquare(A,X,Y,Z):
     maxwave = a[b.index(max(b))]
     minwave = a[b.index(min(b))]
 
-    plt.text(0.3, 0.53, 'maximun wavelength = {:.4f}'.format(maxwave), fontsize=6, transform=plt.gca().transAxes)
-    plt.text(0.3, 0.5, 'minimun wavelength = {:.4f}'.format(minwave), fontsize=6, transform=plt.gca().transAxes)
+    plt.text(0.2, 0.55, 'maximun wavelength = {:.4f}'.format(maxwave), fontsize=6, transform=plt.gca().transAxes)
+    plt.text(0.2, 0.5, 'minimun wavelength = {:.4f}'.format(minwave), fontsize=6, transform=plt.gca().transAxes)
     if max_ind == 9:
         for i in range(-2,1):
             if i==0:
-                plt.text(0.3, 0.45 + 0.05 * i, f'R\u00B2({max_ind + i + 1}deg)= {rs[max_ind + i]}', fontsize=5, transform=plt.gca().transAxes,color='red',weight='bold')
+                plt.text(0.2, 0.45 + 0.05 * i, f'R\u00B2({max_ind + i + 1}deg)= {rs[max_ind + i]}', fontsize=5, transform=plt.gca().transAxes,color='red',weight='bold')
                 continue
-            plt.text(0.3, 0.45+0.05*i, f'R\u00B2({max_ind+i+1}deg)= {rs[max_ind+i]}', fontsize=5, transform=plt.gca().transAxes)
+            plt.text(0.2, 0.45+0.05*i, f'R\u00B2({max_ind+i+1}deg)= {rs[max_ind+i]}', fontsize=5, transform=plt.gca().transAxes)
     else:
         for i in range(-1,2):
             if i==0:
-                plt.text(0.3, 0.45 + 0.05 * (i-1), f'R\u00B2({max_ind + i + 1}deg)= {rs[max_ind + i]}', fontsize=5, transform=plt.gca().transAxes,color='red',weight='bold')
+                plt.text(0.2, 0.45 + 0.05 * (i-1), f'R\u00B2({max_ind + i + 1}deg)= {rs[max_ind + i]}', fontsize=5, transform=plt.gca().transAxes,color='red',weight='bold')
                 continue
-            plt.text(0.3, 0.45+0.05*(i-1), f'R\u00B2({max_ind+i+1}deg)= {rs[max_ind+i]}', fontsize=5, transform=plt.gca().transAxes)
+            plt.text(0.2, 0.45+0.05*(i-1), f'R\u00B2({max_ind+i+1}deg)= {rs[max_ind+i]}', fontsize=5, transform=plt.gca().transAxes)
 
     line, = plt.plot(v[6][0], v[6][1], color='gray', label="REF")
     plt.xticks(fontsize=6)  # modulate axis label's fontsize
@@ -140,7 +140,7 @@ def transmission_rsquare(A,X,Y,Z):
     plt.xlabel('Wavelength [nm]', labelpad=4, fontdict={'weight': 'bold', 'size': 7})  # x축 레이블을 설정
     plt.ylabel('Measured transmission [dB]', labelpad=4, fontdict={'weight': 'bold', 'size': 7})  # y축 레이블을 설정
     plt.gca().add_artist(plt.legend(handles=[line], loc='upper right'))  # REF 레이블을 추가합니다.
-    plt.legend(handles=plots, ncol=3, loc="lower center", fontsize=5)
+    plt.legend(handles=plots, ncol=3, loc="lower left", fontsize=5)
 
     return 0
 
